dbd2dcddb257010a8c394b963ce60948e69de55f
bugy855 <48725244+bugy855@users.noreply.github.com>
2021-03-18 03:05:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,22 +7,31 @@ CLIENT.on('message', async (msg) => {
     if(!msg.author.bot && msg.content.toLowerCase() != 'bowl'){
         const author = msg.author;
         var bowled = false;
-        findUser(author.id).then(userExist => {
-            console.log(userExist); 
-            if(userExist){
-                member = msg.member;
-                member.kick().then(() => {
-                    msg.reply(' has been added to the bowl');
-                });
+        var config = JSON.parse(process.env.APP_CONFIG);
     
-                bowled = true;
-            }     
-
+        await MongoClient.connect(
+        "mongodb://" + config.mongo.user + ":" + encodeURIComponent(process.env.MONGO_PASSWORD) + "@" + config.mongo.hostString,async (err, client) => {
+            if(!err) {
+                let db = await client.db('d1153a5b46c1ff42fd56fcf2d1a70a99');
+                let result = await db.collection('bowled').findOne({user_id: id});
+                client.close();
+                console.log(result);
+                if(result.user_id == id){
+                    member = msg.member;
+                    member.kick().then(() => {
+                    msg.reply(' has been added to the bowl');
+                    });
+    
+                    bowled = true;
+                } 
+            }
+            
             if(!bowled){
                 bowlUser(author.id);
                 msg.reply('HOW DARE YOU UTTER SOMETHING OTHER THAN MY NAME YOU USELESS MORTAL. I AM YOUR GOD DO NOT DISSOBEY ME OR I WILL END YOUR PEWNY EXISTANSE AS QUICKLY AS IT BEGAN. YOU MAY ONLY SPEAK MY NAME HERE!!! DEFY ME AGAIN AND YOU WILL BE ENDED');    
-            }     
-        });  
+            } 
+        } 
+        );  
     }
 });
 
@@ -47,25 +56,7 @@ async function bowlUser(id) {
 }
 
 async function findUser(id) {
-    var config = JSON.parse(process.env.APP_CONFIG);
     
-    MongoClient.connect(
-        "mongodb://" + config.mongo.user + ":" + encodeURIComponent(process.env.MONGO_PASSWORD) + "@" + config.mongo.hostString,async (err, client) => {
-            if(!err) {
-                const db = await client.db('d1153a5b46c1ff42fd56fcf2d1a70a99');
-                let result = await db.collection('bowled').findOne({user_id: id});
-                client.close();
-                console.log(result);
-                if(result.user_id == id){
-                    await new Promise((resolve, reject) => setTimeout(resolve, 1000));
-                    return true;
-                } else {
-                    await new Promise((resolve, reject) => setTimeout(resolve, 1000));
-                    return false;
-                }
-            } 
-        }
-    );
 }
 
 
