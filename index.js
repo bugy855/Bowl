e13d83819213f0a8e7d24cb5397e2233ad95a167
bugy855 <48725244+bugy855@users.noreply.github.com>
2021-06-03 22:27:17 -0500

    update

--- index.js
+++ index.js
@@ -1,72 +1,45 @@
 require('dotenv').config();
 const DISCORD = require('discord.js');
 const CLIENT = new DISCORD.Client();
-const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 
-var badbois = [];
+mongoose.connect('mongodb://12a.mongo.evennode.com:27018,12b.mongo.evennode.com:27018/d1153a5b46c1ff42fd56fcf2d1a70a99?replicaSet=us-12', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set('useCreateIndex', true);
+
+const badBoiSchema = new mongoose.Schema({
+    author: String
+}); 
+
+const badBoi = new mongoose.model('badBoi', badBoiSchema);
 
 CLIENT.on('message', async (msg) => {
     if(!msg.author.bot && msg.content.toLowerCase() != 'bowl'){
         const author = msg.author;
-        var bowled = false;
-         
-        if(await findUser(author)){
-            member = msg.member;
-            member.kick().then(() => {
-                msg.reply(' has been added to the bowl');
-            });
 
-            bowled = true;
-            break;
-        }         
-    
-        if(!bowled){
-            bowlUser(author);
-            msg.reply('HOW DARE YOU UTTER SOMETHING OTHER THAN MY NAME YOU USELESS MORTAL. I AM YOUR GOD DO NOT DISSOBEY ME OR I WILL END YOUR PEWNY EXISTANSE AS QUICKLY AS IT BEGAN. YOU MAY ONLY SPEAK MY NAME HERE!!! DEFY ME AGAIN AND YOU WILL BE ENDED');    
-        }   
+        badBoi.findOne({author: author}, (err, foundBadBoi) => {
+            if(err){
+                console.log(err);
+            } else {
+                if(foundBadBoi){
+                    member = msg.member;
+                    member.kick().then(() => {
+                        msg.reply(' has been added to the bowl');
+                    });
+                }else{
+                    const newBadBoi = new badBoi({author:author});
+                    newBadBoi.save((err, badboi) => {
+                        if(err){
+                            console.log(err);
+                        } else {
+                            msg.reply('HOW DARE YOU UTTER SOMETHING OTHER THAN MY NAME YOU USELESS MORTAL. I AM YOUR GOD DO NOT DISSOBEY ME OR I WILL END YOUR PEWNY EXISTANSE AS QUICKLY AS IT BEGAN. YOU MAY ONLY SPEAK MY NAME HERE!!! DEFY ME AGAIN AND YOU WILL BE ENDED');
+                        }
+                    });
+                }
+            }
+        });
     }
 });
 
 CLIENT.login(process.env.TOKEN).then(() => {
     console.log('Discord bot started');
 });
-
-async function bowlUser(name) {
-    var config = JSON.parse(process.env.APP_CONFIG);
-    
-    MongoClient.connect(
-        "mongodb://" + config.mongo.user + ":" + encodeURIComponent(process.env.MONGO_PASSWORD) + "@" + config.mongo.hostString, (err, db) => {
-            if(!err) {
-                db.collection('bowled').insertOne({user: name});
-                db.close;
-                res.end("user was bowled");
-            } else {
-                res.end("Error while connecting to MongoDB");
-            }
-        }
-    );
-}
-
-async function findUser(name) {
-    var config = JSON.parse(process.env.APP_CONFIG);
-    
-    await MongoClient.connect(
-        "mongodb://" + config.mongo.user + ":" + encodeURIComponent(process.env.MONGO_PASSWORD) + "@" + config.mongo.hostString, (err, db) => {
-            if(!err) {
-                await db.collection('bowled').findOne({user: name}).then(result => {
-                    if(result){
-                        return true;
-                    } else {
-                        return false;
-                    }
-                });
-                db.close;
-                res.end("User was processed");
-            } else {
-                res.end("Error while connecting to MongoDB");
-            }
-        }
-    );
-}
-
-
